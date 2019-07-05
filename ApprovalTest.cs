3c0be87aac78c5a1d200b75fba8cdba7ba5ea693
Jesus Maria Escudero Justel <jm.escudero.justel@gmail.com>
2019-07-05 17:47:22 +0200

    Starting point

---
+++ ApprovalTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Xunit;
+
+namespace gildedroserefactoringkata
+{
+    public class ApprovalTest
+    {
+        [Fact]
+        public void ThirtyDays()
+        {
+            var lines = File.ReadAllLines("ThirtyDays.txt");
+
+            StringBuilder fakeoutput = new StringBuilder();
+            Console.SetOut(new StringWriter(fakeoutput));
+            Console.SetIn(new StringReader($"a{Environment.NewLine}"));
+
+            Program.Main(new string[] { });
+            String output = fakeoutput.ToString();
+
+            var outputLines = output.Split(Environment.NewLine);
+            for(var i = 0; i<Math.Min(lines.Length, outputLines.Length); i++) 
+            {
+                Assert.Equal(lines[i], outputLines[i]);
+            }
+        }
+    }
+}
